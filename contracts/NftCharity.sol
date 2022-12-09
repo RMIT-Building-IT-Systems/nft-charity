a6ac9327e538610dbdce78fd509afa58d9ac5110
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2022-12-09 21:06:25 +0700

    adding decline and reject requests functions

--- contracts/NftCharity.sol
+++ contracts/NftCharity.sol
@@ -6,14 +6,20 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract NftCharity is ReentrancyGuard {
+    enum RequestState {
+        ACTIVE,
+        COMPLETED,
+        REJECTED
+    }
+
     struct Request {
         uint256 id;
         string description;
         uint256 value;
         address recipient;
-        bool complete;
+        RequestState state;
         uint256 approvalsCount;
-        mapping(address => bool) approvals;
+        uint256 declinesCount;
     }
 
     event ItemListed(
@@ -34,11 +40,14 @@ contract NftCharity is ReentrancyGuard {
 
     address private i_admin;
     mapping(address => mapping(uint256 => uint256)) private s_nftListings;
+    mapping(uint256 => mapping(address => bool)) private s_approvals;
+    mapping(uint256 => mapping(address => bool)) s_declines;
     Request[] private requests;
     mapping(address => bool) private s_donators;
     uint256 private s_requestsCount;
     uint256 private s_donatorsCount;
     uint256 private s_completeRequestsCount;
+    uint256 private s_rejectRequestsCount;
     uint256 private s_moneyAvailableForRequests;
 
     modifier onlyAdmin() {
@@ -123,33 +132,56 @@ contract NftCharity is ReentrancyGuard {
         newRequest.description = description;
         newRequest.value = value;
         newRequest.recipient = recipient;
-        newRequest.complete = false;
+        newRequest.state = RequestState.ACTIVE;
         newRequest.approvalsCount = 0;
 
         s_requestsCount++;
         s_moneyAvailableForRequests -= value;
     }
 
-    function approveRequest(uint256 index) public onlyDonator {
+    function approveRequest(uint256 requestId, uint256 index) public onlyDonator {
         Request storage request = requests[index];
 
-        require(!request.approvals[msg.sender], "You have already approved this request");
+        require(request.state == RequestState.ACTIVE, "Request is not active");
+        require(!s_approvals[requestId][msg.sender], "You have already approved this request");
+        require(!s_declines[requestId][msg.sender], "You have already declined this request");
 
-        request.approvals[msg.sender] = true;
+        s_approvals[requestId][msg.sender] = true;
         request.approvalsCount++;
     }
 
-    function finalizeRequest(uint256 index) public onlyAdmin {
+    function declineRequest(uint256 requestId, uint256 index) public onlyDonator {
+        Request storage request = requests[index];
+
+        require(request.state == RequestState.ACTIVE, "Request is not active");
+        require(!s_approvals[requestId][msg.sender], "You have already approved this request");
+        require(!s_declines[requestId][msg.sender], "You have already declined this request");
+
+        s_declines[requestId][msg.sender] = true;
+        request.declinesCount++;
+    }
+
+    function completeRequest(uint256 index) public onlyAdmin {
         Request storage request = requests[index];
 
         require(request.approvalsCount >= (s_donatorsCount / 2), "Not enough approvals");
-        require(!request.complete, "Request already completed");
+        require(request.state == RequestState.ACTIVE, "Request is not active");
 
         (bool success, ) = payable(request.recipient).call{value: request.value}("");
         require(success, "Transfer failed");
-        request.complete = true;
+        request.state = RequestState.COMPLETED;
         s_moneyAvailableForRequests -= request.value;
         s_completeRequestsCount++;
+    }
+
+    function rejectRequest(uint256 index) public onlyAdmin {
+        Request storage request = requests[index];
+
+        require(request.declinesCount >= (s_donatorsCount / 2), "Not enough declines");
+        require(request.state == RequestState.ACTIVE, "Request is not active");
+
+        request.state = RequestState.REJECTED;
+        s_rejectRequestsCount++;
     }
 
     // Getter functions
@@ -158,8 +190,54 @@ contract NftCharity is ReentrancyGuard {
         return i_admin;
     }
 
-    function getNftListingPrice(address nftAddress, uint256 tokenId) external view returns (uint256) {
+    function getNftListingPrice(address nftAddress, uint256 tokenId)
+        external
+        view
+        returns (uint256)
+    {
         return s_nftListings[nftAddress][tokenId];
+    }
+
+    function getRequests() external view returns (Request[] memory) {
+        return requests;
+    }
+
+    function getActiveRequests() external view returns (Request[] memory) {
+        Request[] memory activeRequests = new Request[](
+            s_requestsCount - s_completeRequestsCount - s_rejectRequestsCount
+        );
+        uint256 index = 0;
+        for (uint256 i = 0; i < s_requestsCount; i++) {
+            if (requests[i].state == RequestState.ACTIVE) {
+                activeRequests[index] = requests[i];
+                index++;
+            }
+        }
+        return activeRequests;
+    }
+
+    function getCompletedRequests() external view returns (Request[] memory) {
+        Request[] memory completedRequests = new Request[](s_completeRequestsCount);
+        uint256 index = 0;
+        for (uint256 i = 0; i < s_requestsCount; i++) {
+            if (requests[i].state == RequestState.COMPLETED) {
+                completedRequests[index] = requests[i];
+                index++;
+            }
+        }
+        return completedRequests;
+    }
+
+    function getRejectedRequests() external view returns (Request[] memory) {
+        Request[] memory rejectedRequests = new Request[](s_rejectRequestsCount);
+        uint256 index = 0;
+        for (uint256 i = 0; i < s_requestsCount; i++) {
+            if (requests[i].state == RequestState.REJECTED) {
+                rejectedRequests[index] = requests[i];
+                index++;
+            }
+        }
+        return rejectedRequests;
     }
 
     function getRequestsCount() external view returns (uint256) {
@@ -172,6 +250,10 @@ contract NftCharity is ReentrancyGuard {
 
     function getCompleteRequestsCount() external view returns (uint256) {
         return s_completeRequestsCount;
+    }
+
+    function getRejectedRequestsCount() external view returns (uint256) {
+        return s_rejectRequestsCount;
     }
 
     function getMoneyAvailableForRequests() external view returns (uint256) {
