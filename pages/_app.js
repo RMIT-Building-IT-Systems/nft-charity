1e9b0706e1f82ede0720459fdeb6948ecc6b5234
tamkieu <tamkieu4299@gmail.com>
2022-12-14 23:39:59 +0700

    fix conflict

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/app.css";
+import "../styles/home-page.css";
+import "../styles/about-us.css";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
