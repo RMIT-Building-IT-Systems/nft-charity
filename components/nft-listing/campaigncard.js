f66d1127c5842a02a0a77d508bf41095d1e35da6
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2022-12-30 12:57:34 +0700

    cancel and purchase NFTs

--- components/nft-listing/campaigncard.js
+++ components/nft-listing/campaigncard.js
@@ -1,15 +1,26 @@
 import { useState, useEffect } from "react";
 import { Card, Button } from "antd";
 import * as ethers from "ethers";
-import { useWeb3Contract } from "react-moralis";
 const { Meta } = Card;
 
 import useAdmin from "../../hooks/useAdmin";
 import { nftContractAbi } from "../../constants/ethereum/nftContract";
 
+import { useWeb3Contract } from "react-moralis";
+import {
+    nftCharityContractAddr,
+    nftCharityContractAbi,
+} from "../../constants/ethereum/nftCharityContract";
+import useNotification from "../../hooks/useNotification";
+
 export default function CampaignCard({ price, nftAddress, tokenId }) {
     const [imageUri, setImageUri] = useState("");
+    const [isCancelling, setIsCancelling] = useState(false);
+    const [isPurchasing, setIsPurchasing] = useState(false);
+
     const { isAdmin } = useAdmin();
+    const { raiseSuccess, raiseFailure, notificationContextHolder } = useNotification();
+
     const { runContractFunction: getTokenURI } = useWeb3Contract({
         abi: nftContractAbi,
         contractAddress: nftAddress,
@@ -30,24 +41,91 @@ export default function CampaignCard({ price, nftAddress, tokenId }) {
         }
     };
 
+    const { runContractFunction } = useWeb3Contract();
+
+    const cancelListing = async () => {
+        setIsCancelling(true);
+        const cancelOptions = {
+            abi: nftCharityContractAbi,
+            contractAddress: nftCharityContractAddr,
+            functionName: "cancelListing",
+            params: {
+                nftAddress: nftAddress,
+                tokenId: tokenId,
+            },
+        };
+
+        await runContractFunction({
+            params: cancelOptions,
+            onSuccess: (tx) => {
+                tx.wait();
+                raiseSuccess("Successfully removed NFT!");
+            },
+            onError: (error) => {
+                console.log(error);
+                raiseFailure("Error removing NFTs!");
+            },
+        });
+        setIsCancelling(false);
+    };
+
+    const purchaseItem = async () => {
+        setIsPurchasing(true);
+        const purchaseOptions = {
+            abi: nftCharityContractAbi,
+            contractAddress: nftCharityContractAddr,
+            functionName: "buyItem",
+            msgValue: price,
+            params: {
+                nftAddress: nftAddress,
+                tokenId: tokenId,
+            },
+        };
+
+        await runContractFunction({
+            params: purchaseOptions,
+            onSuccess: (tx) => {
+                tx.wait();
+                raiseSuccess("Successfully purchased NFT!");
+            },
+            onError: (error) => {
+                console.log(error);
+                raiseFailure("Error purchasing NFTs!");
+            },
+        });
+        setIsPurchasing(false);
+    };
+
     useEffect(() => {
         updateUI();
     }, [updateUI]);
 
     return (
-        <Card
-            cover={
-                imageUri ? <img style={{ height: "250px" }} alt="example" src={imageUri} /> : null
-            }
-            hoverable
-        >
-            <Meta description={`Price: ${ethers.utils.formatEther(price)} ETH`} />
-            <br />
-            <div className="addButton">
-                <Button danger={isAdmin} style={{ height: "40px" }} type="primary" block>
-                    {isAdmin ? "Cancel" : "Purchase"}
-                </Button>
-            </div>
-        </Card>
+        <>
+            {notificationContextHolder}
+            <Card
+                cover={
+                    imageUri ? (
+                        <img style={{ height: "250px" }} alt="example" src={imageUri} />
+                    ) : null
+                }
+                hoverable
+            >
+                <Meta description={`Price: ${ethers.utils.formatEther(price)} ETH`} />
+                <br />
+                <div className="addButton">
+                    <Button
+                        loading={isCancelling || isPurchasing}
+                        danger={isAdmin}
+                        style={{ height: "40px" }}
+                        onClick={isAdmin ? cancelListing : purchaseItem}
+                        type="primary"
+                        block
+                    >
+                        {isAdmin ? "Cancel" : "Purchase"}
+                    </Button>
+                </div>
+            </Card>
+        </>
     );
 }
