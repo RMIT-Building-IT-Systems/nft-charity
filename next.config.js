603f73bdc549f50bc01453a6da37ed4f1f8f2d81
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-01-05 13:35:16 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MAINNET_RPC_URL: process.env.MAINNET_RPC_URL,
+    GOERLI_RPC_URL: process.env.GOERLI_RPC_URL,
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
+    ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
+    PINATA_API_KEY: process.env.PINATA_API_KEY,
+    PINATA_API_SECRET: process.env.PINATA_API_SECRET,
+  }
 }
 
 module.exports = nextConfig
